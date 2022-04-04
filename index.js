cb7962e2becaebd9df2047c262c7570896455048
gowtham <gowtham3798@gmail.com>
2022-04-04 13:35:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import Razorpay from "razorpay";
+import shortid from "shortid";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import fs from 'fs';
+
+dotenv.config()
+const app = express();
+const port = 4000;
+app.use(cors());
+app.use(bodyParser.json())
+
+var razorpay = new Razorpay({
+    key_id : process.env.KEY_ID,
+    key_secret : process.env.KEY_SECRET
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome✅😂')
+})
+
+app.post('/razorpay',async (req, res) => {
+    const payment_capture = 1;
+    const amount = 500;
+    const currency = 'INR'
+    const options = {
+        amount : (amount*100),
+        currency,
+        receipt:shortid.generate(),
+        payment_capture
+    }
+    try{
+        const response = await razorpay.orders.create(options)
+        console.log(response)
+        res.json({
+            id : response.id,
+            currency : response.currency,
+            amount : response.amount
+        })
+    }
+     catch(err){
+         console.log(err)
+     }
+})
+
+app.listen(port , () => {
+    console.log("Listening on port 4000")
+})
